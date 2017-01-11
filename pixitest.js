a9c0786de4741c94d516cbf0176147e745a54d36
Dave Figueroa <davefig@gmail.com>
2017-01-10 20:58:12 -0500

    Simple Movement of a Sprite

--- pixitest.js
+++ pixitest.js
@@ -12,17 +12,45 @@ PIXI.loader
   .add("img/Lotso_Bear.png")
   .load(setup);
 
+var bear;
+
 //This `setup` function will run when the image has loaded
 function setup() {
 
   //Create the sprite from the texture
-  var bear = new PIXI.Sprite(
+  bear = new PIXI.Sprite(
     PIXI.loader.resources["img/Lotso_Bear.png"].texture
   );
+
+  //Initialize the bear's velocity variables
+  bear.vx = 0;
+  bear.vy = 0;
+
+  //Adjust the sprite's size and position
+  bear.scale.set(0.25, 0.25);
+  bear.position.set(90,90);
 
   //Add the sprite to the stage
   stage.addChild(bear);
 
-  //Tell the `renderer` to `render` the `stage`
+  //Start the game loop
+  gameLoop();
+}
+
+function gameLoop() {
+
+  //Loop this function at 60 frames per second
+  requestAnimationFrame(gameLoop);
+
+  //Update the cat's velocity
+  bear.vx = 1;
+  bear.vy = 1;
+
+  //Apply the velocity values to the cat's
+  //position to make it move
+  bear.x += bear.vx;
+  bear.y += bear.vy;
+
+  //Render the stage to see the animation
   renderer.render(stage);
 }
